3c1104bb7355837929e3e714db466e40d3ece75a
Jason Yi <j.hyonyi@gmail.com>
2025-12-25 22:43:27 +0000

    add autopair plugin, set tabs=4

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 require("config.keymaps")
-
 require("config.lazy")
 
 -- Hybrid Line Numbers
 vim.opt.number = true         -- Show line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
+
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4      -- Width of a tab character
+vim.opt.shiftwidth = 4   -- Size of an indent (when you hit Tab)
+vim.opt.expandtab = true -- Convert tabs to spaces (recommended)
+vim.opt.softtabstop = 4  -- Makes Backspace delete 4 spaces like a Tab
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,53 +1,60 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",     -- Source for LSP suggestions
-        "L3MON4D3/LuaSnip",         -- Snippet engine (Required for cmp to work!)
-        "saadparwaiz1/cmp_luasnip", -- Source for snippets
-    },
-    config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp", -- Source for LSP suggestions
+		"L3MON4D3/LuaSnip", -- Snippet engine
+		"saadparwaiz1/cmp_luasnip", -- Source for snippets
+		"windwp/nvim-autopairs", -- Install autopairs
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                -- TAB to select next suggestion
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
+		-- <--- ADDED: Load autopairs integration
+		local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
-                -- SHIFT + TAB to select previous suggestion
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				['<Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
 
-                -- ENTER to confirm selection
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+				['<S-Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
 
-                -- CTRL + SPACE to trigger menu manually
-                ['<C-Space>'] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' }, -- Get suggestions from LSP (clangd, rust_analyzer)
-                { name = 'luasnip' },  -- Get suggestions from snippets
-            })
-        })
-    end
+				['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+				['<C-Space>'] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+			})
+		})
+
+		cmp.event:on(
+			'confirm_done',
+			cmp_autopairs.on_confirm_done()
+		)
+
+		require("nvim-autopairs").setup({})
+	end
 }
