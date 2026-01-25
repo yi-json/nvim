7abb310346e1ccead88a43e58c87583af1d2b13a
Jason Yi <j.hyonyi@gmail.com>
2026-01-25 14:54:27 -0500

    change cpp formatting to be 4 spaces per tab

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,31 +1,39 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				-- Use "prettier" for web stuff
-				javascript = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
+        conform.setup({
+            formatters_by_ft = {
+                -- Use "prettier" for web stuff
+                javascript = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
 
-				-- Use "stylua" for Lua
-				lua = { "stylua" },
-				cpp = { "clang-format" },
-				c = { "clang-format" },
-				rust = { "rustfmt" }
-			},
+                -- Use "stylua" for Lua
+                lua = { "stylua" },
+                cpp = { "clang-format" },
+                c = { "clang-format" },
+                rust = { "rustfmt" }
+            },
 
-			-- Set up Format on Save
-			format_on_save = {
-				lsp_fallback = true, -- If no formatter is found, use the LSP (Essential for C++/Rust/Go!)
-				async = false,
-				timeout_ms = 500,
-			},
-		})
-	end
+            formatters = {
+                ["clang-format"] = {
+                    -- "IndentWidth: 4" forces 4 spaces.
+                    -- "UseTab: Never" ensures it uses spaces, not \t characters.
+                    prepend_args = { "-style={BasedOnStyle: LLVM, IndentWidth: 4, UseTab: Never}" },
+                },
+            },
+
+            -- Set up Format on Save
+            format_on_save = {
+                lsp_fallback = true, -- If no formatter is found, use the LSP (Essential for C++/Rust/Go!)
+                async = false,
+                timeout_ms = 500,
+            },
+        })
+    end
 }
